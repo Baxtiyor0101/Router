043dc5b23ee21f979f29f1cf1783ab7202a24986
Baxtiyor <oqonboyevb@gmail.com>
2022-01-18 23:08:38 +0500

    router6

---
+++ src/mock/nav2data.js
@@ -0,0 +1,12 @@
+export const data = [
+    "Kino",
+    "Comedy",
+    "Mix",
+    "Cartoons",
+    "Misic",
+    "Football",
+    "UX Tasarim",
+    "Animations",
+    "Best Arts",
+    "Last Updates",
+  ]
